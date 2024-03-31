fd6b324dc75834fb47b0ea023514f537aa3a43a6
marconesb <marconebritt@gmail.com>
2024-03-31 20:16:52 -0300

    alteração da img

--- menu.js
+++ menu.js
@@ -1,19 +1,19 @@
+
 document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.getElementById('menu-toggle');
   const menu = document.getElementById('menu');
   const overlay = document.getElementById('overlay');
 
   menuToggle.addEventListener('click', () => {
-      // Alternar visibilidade do menu
-      if (menu.style.display === 'none' || menu.style.display === '') {
-          menu.style.display = 'block';
-          overlay.style.display = 'block';
-          menuToggle.classList.add('open'); // Adiciona a classe 'open' para animar o ícone do menu
-          menu.style.backgroundColor = '#3498db'; // Define a cor de fundo quando o menu é exibido
+      menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+      overlay.style.display = overlay.style.display === 'none' ? 'block' : 'none';
+      menuToggle.classList.toggle('open'); // Adiciona ou remove a classe 'open' para animar o ícone do menu
+
+      // Alterando o background do menu
+      if (menu.style.display === 'block') {
+          menu.style.backgroundColor = '#ffffff'; // Cor de fundo do menu quando é exibido
       } else {
-          menu.style.display = 'none';
-          overlay.style.display = 'none';
-          menuToggle.classList.remove('open'); // Remove a classe 'open' para animar o ícone do menu
+          menu.style.backgroundColor = ''; // Remove o background personalizado quando o menu é ocultado
       }
   });
 
