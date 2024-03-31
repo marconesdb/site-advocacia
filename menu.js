43ba7bfa853d3e1696d9dc2117bd07f1591c9de4
marconesb <marconebritt@gmail.com>
2024-03-31 19:01:06 -0300

    alteração da cor do menu

--- menu.js
+++ menu.js
@@ -1,27 +1,34 @@
- // JavaScript para controlar a exibição do menu
- document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
-    const overlay = document.getElementById('overlay');
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+  const overlay = document.getElementById('overlay');
 
-    menuToggle.addEventListener('click', () => {
-      menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
-      overlay.style.display = overlay.style.display === 'none' ? 'block' : 'none';
-      menuToggle.classList.toggle('open'); // Adiciona ou remove a classe 'open' para animar o ícone do menu
-    });
+  menuToggle.addEventListener('click', () => {
+      // Alternar visibilidade do menu
+      if (menu.style.display === 'none' || menu.style.display === '') {
+          menu.style.display = 'block';
+          overlay.style.display = 'block';
+          menuToggle.classList.add('open'); // Adiciona a classe 'open' para animar o ícone do menu
+          menu.style.backgroundColor = '#3498db'; // Define a cor de fundo quando o menu é exibido
+      } else {
+          menu.style.display = 'none';
+          overlay.style.display = 'none';
+          menuToggle.classList.remove('open'); // Remove a classe 'open' para animar o ícone do menu
+      }
+  });
 
-    overlay.addEventListener('click', () => {
+  overlay.addEventListener('click', () => {
       menu.style.display = 'none';
       overlay.style.display = 'none';
       menuToggle.classList.remove('open'); // Remove a classe 'open' para animar o ícone do menu
-    });
-
-    // Fechar o menu ao clicar fora dele
-    document.addEventListener('click', (event) => {
-      if (!menu.contains(event.target) && event.target !== menuToggle) {
-        menu.style.display = 'none';
-        overlay.style.display = 'none';
-        menuToggle.classList.remove('open'); // Remove a classe 'open' para animar o ícone do menu
-      }
-    });
   });
+
+  // Fechar o menu ao clicar fora dele
+  document.addEventListener('click', (event) => {
+      if (!menu.contains(event.target) && event.target !== menuToggle) {
+          menu.style.display = 'none';
+          overlay.style.display = 'none';
+          menuToggle.classList.remove('open'); // Remove a classe 'open' para animar o ícone do menu
+      }
+  });
+});
